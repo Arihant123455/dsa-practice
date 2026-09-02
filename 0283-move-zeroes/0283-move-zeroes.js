dface5fb07e2b0ea1080bbcbcb9ae5f2354ac146
Arihant Jain <116261090+Arihant123455@users.noreply.github.com>
2026-09-02 23:12:09 +0530

    Time: 4 ms (30.12%), Space: 60.7 MB (36.46%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+
+    let n = nums.length;
+
+    if(n === 0)
+    return;
+
+    let j = -1;
+    for(let i = 0; i < n; i++) 
+    {
+        if(nums[i] === 0)
+        {
+            j = i;
+            break;
+
+        }
+    }
+    if(j === -1)
+    return;
+
+    for(let i = j+1; i < n; i++)
+    {
+        if(nums[i] !== 0)
+        {
+            [nums[j], nums[i]] = [nums[i], nums[j]];
+            j++;
+        }
+    }
+};
