685c101593c197ac48d19f02dd2e8fe75417a45b
Arihant Jain <116261090+Arihant123455@users.noreply.github.com>
2026-09-11 00:46:48 +0530

    Time: 1 ms (77.14%), Space: 55.8 MB (29.72%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -4,18 +4,21 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
- const mp = {};
-    for (const num of nums1) {
+
+    const mp = {};
+    for(const num of nums1)
+    {
         mp[num] = (mp[num] || 0) + 1;
     }
-    
-    const result = [];
-    for (const num of nums2) {
-        if (mp[num]) {
-            result.push(num);
+
+    const insertion = [];
+    for(const num of nums2)
+    {
+        if(mp[num])
+        {
+            insertion.push(num);
             delete mp[num];
         }
     }
-    
-    return result;
+    return insertion;
 };
